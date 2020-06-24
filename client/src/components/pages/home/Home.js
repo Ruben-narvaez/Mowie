7f6c854b8d9acbf93b0355a540bcc90c80ef337b
Ruben Narvaez <rubnarvaez@gmail.com>
2020-06-24 16:42:08 +0200

    Homepage changed but not 100%

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Videographer from '../../ui/ImagesforCol/photographer-colour-800px.png'
+import Filmmaker from '../../ui/ImagesforCol/undraw_videographer_nnc7.svg'
+import Connect from '../../ui/ImagesforCol/handshake-colour.svg'
+import Role from '../../ui/ImagesforCol/mirror-pose-colour.svg'
+import ProjectShare from '../../ui/ImagesforCol/drawkit-content-man-colour.svg'
+
 
 import './Home.css'
 
@@ -10,30 +16,44 @@ const Home = () => {
     return (
        
         <Container fluid={true}>
-            <Row className="homePhrase">
+            <Row className="homePhrase backImg1">
                 <Col className="colPhrase" md="6">
-                    <section>                       
+                    <section>   
                         <h1>La red social de los filmmakers independientes</h1>
                     </section>
                 <Link className="homeButton" to="/signup" >Crea tu cuenta</Link>
                 </Col>
-                <Col className="travolta" md="6">
-                    <section>
-                    {/* <img src="./../ui/ImageFoBack/travol.png" alt="travolta" /> */}
-                    </section>
+                <Col md="6">
+                    <img src={Videographer} alt="Videographer" />
                 </Col>
             </Row>
-            <Row className="blueRow">
-                <Col className="miaWallace" md="6">
-                </Col>
+            <Row className="backImg2">
+                <Col md="6"></Col>
                 <Col className="bluePhrase" md="6">
-                    <h2>Únete, determina tu rol y colabora en los proyectos de la comunidad, o busca compañeros para tu propios proyectos.</h2>
+                    <h2>Únete, determina tu rol y colabora en los proyectos de la comunidad</h2>
                     <Link className="loginButton" to="/login" >Entra</Link>
                 </Col>
             </Row>
-            {/* <Row className="homeFooter">
-                <p>Made by Rub</p>
-            </Row>  */}
+            <Row className="blueRow">
+                <Col className="colBlue role" md="4">
+                    <h4>Determina tu rol</h4>
+                    <img src={Role} alt="Rol" />
+                </Col>
+                <Col className="colBlue" md="4">
+                    <h4>Busca compañeros</h4>
+                    <img src={Connect} alt="Conecta" />
+                </Col>
+                <Col className="colBlue" md="4">
+                    <h4>Comparte tus proyectos</h4>
+                    <img src={ProjectShare} alt="Project Sharing" />
+                </Col>
+            </Row>
+            <Row className="lastRow backImg3">
+                <h1>¡Te estamos esperando!</h1>
+                <Col md="12">
+                    <img src={Filmmaker} alt="filmmaker" />
+                </Col>
+            </Row>
         </Container>
          
     )
